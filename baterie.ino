457e1007eccef5c7cbdea6c56668d9c57eb166d9
stebrony <38429913+stebrony@users.noreply.github.com>
2018-09-03 15:01:20 +0200

    Pokus
    Pokus s napětím.

---
+++ baterie.ino
@@ -0,0 +1,99 @@
+
+// Including the ESP8266 WiFi library
+#include <ESP8266WiFi.h>
+
+// Replace with your network details
+const char* ssid = "...";
+const char* password = "...";
+
+
+ADC_MODE(ADC_VCC);
+char v_str[10];
+
+// Web Server on port 80
+WiFiServer server(80);
+
+// only runs once on boot
+void setup() {
+  // Initializing serial port for debugging purposes
+  Serial.begin(115200);
+  delay(10);
+
+  // Connecting to WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  
+  // Starting the web server
+  server.begin();
+  Serial.println("Web server running. Waiting for the ESP IP...");
+  delay(10000);
+  
+  // Printing the ESP IP address
+  Serial.println(WiFi.localIP());
+}
+
+void getBaterry() {
+   // most exact output
+ // uint16_t v = ESP.getVcc();
+ uint16_t v = analogRead(A0)*0.08;
+ 
+  float_t v_cal = ((float)v/1024.0f);
+  
+  dtostrf(v_cal, 5, 3, v_str);
+  sprintf(v_str,"%s V", v_str);
+  
+}
+
+// runs over and over again
+void loop() {
+  // Listenning for new clients
+  WiFiClient client = server.available();
+  
+  if (client) {
+    Serial.println("New client");
+    // bolean to locate when the http request ends
+    boolean blank_line = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        
+        if (c == '\n' && blank_line) {
+            getBaterry();
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: text/html");
+            client.println("Connection: close");
+            client.println();
+            // your actual web page that displays temperature
+            client.println("<!DOCTYPE HTML>");
+            client.println("<html>");
+            client.println("<head></head><body><h1>ESP8266 - Napětí baterie</h1><h3>Napětí baterie: ");
+            client.println(v_str);
+            
+            break;
+        }
+        if (c == '\n') {
+          // when starts reading a new line
+          blank_line = true;
+        }
+        else if (c != '\r') {
+          // when finds a character on the current line
+          blank_line = false;
+        }
+      }
+    }  
+    // closing the client connection
+    delay(1);
+    client.stop();
+    Serial.println("Client disconnected.");
+  }
+}   
